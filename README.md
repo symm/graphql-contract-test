a9087a0b342cdf8f20edf4e3d7161beab9f75d0f
Gareth Jones <me@gazj.co.uk>
2017-06-11 23:26:12 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,19 @@
+# GraphQL Contract Test
+
+Consumer driven contract testing for GraphQL APIs.
+
+## Usage
+
+```
+graphql-contract-test http://your-server.tld/graphql client-schema.graphql
+```
+
+Where `client-schema.graphql` contains the schema you expect the server to implement.
+
+An [introspection query](http://graphql.org/learn/introspection/) will be made against the API, any breaking changes will be reported
+
+
+## Further Reading
+
+* [Consumer-Driven Contracts: A Service Evolution Pattern](https://www.martinfowler.com/articles/consumerDrivenContracts.html)
+* [Tech Radar Consumer-driven contract testing](https://www.thoughtworks.com/radar/techniques/consumer-driven-contract-testing)
---
+++ src/index.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+
+import { introspectionQuery } from 'graphql/utilities/introspectionQuery';
+import { buildClientSchema } from 'graphql/utilities/buildClientSchema';
+import { findBreakingChanges } from 'graphql/utilities/findBreakingChanges';
+import { buildSchema } from 'graphql';
+
+import fetch from 'node-fetch';
+import Table from 'cli-table';
+import minimist from 'minimist';
+import chalk from 'chalk';
+import fs from 'fs';
+
+const usage = '  Usage: graphql-contract-test ENDPOINT_URL <client_schema_file>';
+const error = chalk.bold.red;
+const success = chalk.bold.green;
+
+async function main() {
+  const argv = minimist(process.argv.slice(2));
+
+  if (argv._.length < 2) {
+    console.log(usage);
+    return;
+  }
+
+  const endpoint = argv._[0];
+  const expectedSchemaFile = argv._[1];
+
+
+  console.log(`[*] Checking ${endpoint} against ${expectedSchemaFile}...\n`);
+
+  let expectedSchema;
+
+  fs.readFile(expectedSchemaFile, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+
+    expectedSchema = buildSchema(data);
+  });
+
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query: introspectionQuery }),
+  });
+
+  const { data, errors } = await response.json();
+
+  if (errors) {
+    throw new Error(JSON.stringify(errors, null, 2));
+  }
+
+  const actualSchema = buildClientSchema(data);
+  const breakingChanges = findBreakingChanges(expectedSchema, actualSchema);
+
+  if (breakingChanges.length === 0) {
+    console.log(success('✨  The server appears to implement the schema you provided'));
+    process.exit(0);
+  }
+
+  console.log(`💩  ${error('Breaking changes were detected\n')}`);
+
+  const table = new Table({
+    head: ['Change Type', 'Description'],
+  });
+
+  breakingChanges.forEach((change) => {
+    table.push([change.type, change.description]);
+  });
+
+  console.log(table.toString());
+
+  process.exit(1);
+}
+
+main().catch(e => console.error(e));
